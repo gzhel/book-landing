690398e5b691d9b7eb2b2173d6713233501a05ec
gregory <gregoryzhel@gmail.com>
2020-09-21 18:55:00 +0300

    update

---
+++ src/components/pages/home/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const HomePage = () => {
+    return(
+        <>
+            This is a home page
+            {/* <Header/>
+            <BannerArea/>
+            <InfoArea/>
+            <FactArea/>
+            <CounterArea/>
+            <PriceArea/>
+            <CourseArea/>
+            <CallToActionArea/>
+            <TestomialArea/>
+            <Footer/> */}
+        </>
+    )
+};
+
+export default HomePage;
