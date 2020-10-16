0b84240cf5d3ecf3fd56a384268cfbd936233fdc
GregoryZhelyabin <52909777+GregoryZhelyabin@users.noreply.github.com>
2020-10-16 16:15:37 +0300

    slider is done

--- src/pages/home/home-feedback-area/feedback-area.component.jsx
+++ src/pages/home/home-feedback-area/feedback-area.component.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+import FirstImage from "../../../shared/images/t1.png";
 
 export const FeedbackAreaComponent = () => {
-  //const SiemaCarousel = new Siema();
+  let settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+  };
 
   return (
-    <section className="feedback-area pb-120">
+    <section className="feedback-area">
       <div className="container">
         <div className="row d-flex justify-content-center">
           <div className="pb-60 col-lg-8">
@@ -14,7 +27,58 @@ export const FeedbackAreaComponent = () => {
             </div>
           </div>
         </div>
-        <div className="row"></div>
+        <div>
+          <Slider {...settings}>
+            <div class="slider-item-layout">
+              <div class="single-testimonial item">
+                <img class="mx-auto" src={FirstImage} alt="" />
+                <p class="desc">
+                  Accessories Here you can find the best computer accessory for
+                  your laptop, monitor, printer, scanner, speaker, projector,
+                  hardware and more. laptop accessory
+                </p>
+                <h4>Mark Alviro Wiens</h4>
+                <p>CEO at Google</p>
+              </div>
+            </div>
+            <div class="slider-item-layout">
+              <div class="single-testimonial item">
+                <img class="mx-auto" src={FirstImage} alt="" />
+                <p class="desc">
+                  Accessories Here you can find the best computer accessory for
+                  your laptop, monitor, printer, scanner, speaker, projector,
+                  hardware and more. laptop accessory
+                </p>
+                <h4>Mark Alviro Wiens</h4>
+                <p>CEO at Google</p>
+              </div>
+            </div>
+            <div class="slider-item-layout">
+              <div class="single-testimonial item">
+                <img class="mx-auto" src={FirstImage} alt="" />
+                <p class="desc">
+                  Accessories Here you can find the best computer accessory for
+                  your laptop, monitor, printer, scanner, speaker, projector,
+                  hardware and more. laptop accessory
+                </p>
+                <h4>Mark Alviro Wiens</h4>
+                <p>CEO at Google</p>
+              </div>
+            </div>
+            <div class="slider-item-layout">
+              <div class="single-testimonial item">
+                <img class="mx-auto" src={FirstImage} alt="" />
+                <p class="desc">
+                  Accessories Here you can find the best computer accessory for
+                  your laptop, monitor, printer, scanner, speaker, projector,
+                  hardware and more. laptop accessory
+                </p>
+                <h4>Mark Alviro Wiens</h4>
+                <p>CEO at Google</p>
+              </div>
+            </div>
+          </Slider>
+        </div>
       </div>
     </section>
   );
