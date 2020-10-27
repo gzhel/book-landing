df0b30a588a8871059ea7b0fda2fba35d3951f7a
gzhel <gregoryzhel@gmail.com>
2020-10-27 19:58:14 +0300

    a lot of new UI/UX features

--- src/pages/home/home-info-area/info-area.component.jsx
+++ src/pages/home/home-info-area/info-area.component.jsx
@@ -5,9 +5,27 @@ export const InfoAreaComponent = () => {
   return (
     <section className={"section-gap info-area"} id="home-about">
       <div className="container">
+        <div className="row d-flex justify-content-center">
+          <div className="pb-60 col-lg-8">
+            <div className="title text-center">
+              <h1 className="mb-10">Some information about me</h1>
+              <p>
+                I am not only a professional IT developer, but also a very
+                creative person.
+              </p>
+            </div>
+          </div>
+        </div>
         <div className="single-info row mt-40 align-items-center">
           <div className="col-lg-6 col-md-12 text-center no-padding info-left">
-            <img src={InfoPhotoImage} alt="Info" />
+            <img
+              src={InfoPhotoImage}
+              width={600}
+              height={600}
+              alt="Info"
+              data-aos="fade"
+              data-aos-duration="800"
+            />
           </div>
           <div className="col-lg-6 col-md-12 no-padding info-right">
             <div
@@ -17,17 +35,21 @@ export const InfoAreaComponent = () => {
             >
               <h2 className="pb-30">Gregory Zhelyabin</h2>
               <p>
-                inappropriate behavior is often laughed off as “boys will be
-                boys,” women face higher conduct standards – especially in the
-                workplace. That’s why it’s crucial that, as women.
+                I was born in Rostov-on-don, Russia. I only started getting
+                interested in IT and programming in high school, when I was 18
+                years old. At the age of 20, I got my first job in IT with
+                minimal knowledge of HTML and CSS. After that, I improved my
+                skills in the shortest possible time, and also learned the
+                basics of JavaScript, TypeScript and React.
               </p>
               <p>
-                inappropriate behavior is often laughed off as “boys will be
-                boys,” women face higher conduct standards – especially in the
-                workplace. That’s why it’s crucial that, as women. inappropriate
-                behavior is often laughed off as “boys will be boys,” women face
-                higher conduct standards – especially in the workplace. That’s
-                why it’s crucial that, as women.
+                Despite not having much experience in programming, I try to pay
+                attention to small things. A good result for me is one in which
+                there are no open questions. In addition to IT, I am interested
+                in creating video games, designing game worlds, characters, and
+                scenarios. I love good movies, and I can easily distinguish a
+                good product from a bad one, as well as a good UI/UX from a bad
+                one.
               </p>
             </div>
           </div>
